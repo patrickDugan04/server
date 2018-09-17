0392b78b4e25192c6dce590e5d872ffc272f7226
Unknown <patrickdugan04@gmail.com>
2018-09-16 20:30:20 -0400

    added writing and reading files

--- exec.php
+++ exec.php
@@ -5,12 +5,21 @@
 		<?php
 			$prev = $_POST["prev"];
 			$insert = $_POST["insert"];
+			$write = $_POST["write"];
+			$file = $_POST["file"];
+			echo $file;
 			if($prev == ""){
 				$in = $insert;
 			} else {
 				$in = $prev . "; " . $insert;							}
 			echo "<h2>$in</h2><br><br>";
-			
+			if($write == ""){
+				echo readfile($file);
+			}else{
+				$myfile = fopen($file, "w") or die("Unable to open 				file!");
+				fwrite($myfile, $write);
+				fclose($myfile);
+			}
 		?>
  
 	</head>
@@ -25,10 +34,13 @@
 		<form action = "/exec.php" method="post">
 			<input type="text" name="insert">
 			<input type="hidden" name="prev" value = '<?php echo "$in"; ?>'/>
-			<input type="submit" value="continue commands">
+			<input type="submit" value="submit"><br>
+			leave write blank to read file<br>
+			<input type = "text" name = "file"><br>
+			write<br>
+			<input type = "text" name = "write">
 		</form>
 		<form action = "/exec.php" method="post">
-			<input type="hidden" name="prev" value = ''>
 			<input type="submit" value="reset">
 		</form>
 
